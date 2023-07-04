597c04cf555df164370352f68dd835a6f89cee2b
Nikola Cerovina <cerovina1997@gmail.com>
2023-07-05 01:52:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("but1").addEventListener("click", function() {
+    alert("You searched!");
+  });
+  
+  document.getElementById("but2").addEventListener("click", function() {
+    alert("You opened the menu!");
+  });
+  
+  document.getElementById("but3").addEventListener("click", function() {
+    alert("You liked it!");
+  });
+  
+  document.getElementById("but4").addEventListener("click", function() {
+    alert("You wrote a comment!");
+  });
+  
+  document.getElementById("but5").addEventListener("click", function() {
+    alert("You shared it!");
+  });
